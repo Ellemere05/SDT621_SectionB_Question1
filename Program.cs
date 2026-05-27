34f7f5bac8d64a3248f0c249516c7d10615bb9b1
Ellemere05 <160120660+Ellemere05@users.noreply.github.com>
2026-05-27 09:28:45 +0200

    Finished

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the Structure of a C# Program Demo!");
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}!\n");
+
+            Console.WriteLine("Program Structure Demonstrated:" +
+                "\n1. using System : imports functionality" +
+                "\n2. namespace : organizes code" +
+                "\n3. class Program : container for code" +
+                "\n4. Main() : entry point of program" +
+                "\n5. Comments :  explain logic and documentation\n" +
+                "\nProgram Executed Successfully!");
         }
     }
 }
