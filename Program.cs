2862135d5b7dd490b7aa5c550dd79c3e29227cc6
Ellemere05 <160120660+Ellemere05@users.noreply.github.com>
2026-05-27 09:22:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SDT621_SectionB_Question1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
